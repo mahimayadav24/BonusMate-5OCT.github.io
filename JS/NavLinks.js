cb31e7360131fe9d8fdca5aa4f8e171379695821
mahimayadav24 <88049838+mahimayadav24@users.noreply.github.com>
2021-10-05 17:55:18 +0530

    HTML

---
+++ JS/NavLinks.js
@@ -0,0 +1,8 @@
+$(function ($) {
+    let url = window.location.href;
+    $('li a').each(function () {
+        if (this.href === url) {
+            $(this).closest('li').addClass('activa');
+        }
+    });
+});
